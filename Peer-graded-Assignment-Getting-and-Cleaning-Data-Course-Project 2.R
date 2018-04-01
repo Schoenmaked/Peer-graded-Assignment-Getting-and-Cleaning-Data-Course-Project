920c91bda8dbaa5eb959ed4cc3044a41b5269326
Schoenmaked <35661943+Schoenmaked@users.noreply.github.com>
2018-04-01 14:36:07 +0200

    Add files via upload

---
+++ Peer-graded-Assignment-Getting-and-Cleaning-Data-Course-Project 2.R
@@ -0,0 +1,46 @@
+if(!file.exists("./data")){dir.create("./data")}
+#Load data into the directory:
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(fileUrl,destfile="./data/Dataset.zip")
+
+# Unzip dataSet to /data directory
+unzip(zipfile="./data/Dataset.zip",exdir="./data")
+
+# read train data
+X_train <- read.table("./data/UCI HAR Dataset/train/X_train.txt")
+Y_train <- read.table("./data/UCI HAR Dataset/train/y_train.txt")
+subject_train <- read.table("./data/UCI HAR Dataset/train/subject_train.txt")
+
+# reading training tables
+X_test <- read.table("./data/UCI HAR Dataset/test/X_test.txt")
+Y_test <- read.table("./data/UCI HAR Dataset/test/y_test.txt")
+subject_test <- read.table("./data/UCI HAR Dataset/test/subject_test.txt")
+
+# Reading features
+features <- read.table('./data/UCI HAR Dataset/features.txt')
+
+# read activity labels
+activity_labels = read.table('./data/UCI HAR Dataset/activity_labels.txt')
+
+# 1. Merges all dat a in one set 
+X_total <- rbind(X_train, X_test)
+Y_total <- rbind(Y_train, Y_test)
+Sub_total <- rbind(subject_train, subject_test)
+
+# 2. Extracts only the measurements on the mean and standard deviation for each measurement.
+MeanStd <- features[grep("mean\\(\\)|std\\(\\)",features[,2]),]
+X_total <- X_total[,MeanStd[,1]]
+
+# 3. Uses descriptive activity names to name the activities in the data set
+colnames(Y_total) <- "activity"
+Y_total$activitylabel <- factor(Y_total$activity, labels = as.character(activity_labels[,2]))
+activity_labels<- Y_total[,-1]
+
+# 4. Appropriately labels the data set with descriptive variable names.
+colnames(X_total) <- features[MeanStd[,1],2]
+
+# 5. From the data set in step 4, creates a second, independent tidy data set with the average
+# of each variable for each activity and each subject.
+colnames(Sub_total) <- "subject"
+total <- cbind(X_total, activity_labels, Sub_total)
+write.table(total, file = "./data/UCI HAR Dataset/tidydata.txt", row.names = FALSE, col.names = TRUE)
